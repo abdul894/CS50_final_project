9af125059d339693aa543505238ae066fbefc268
abdul894 <a.rehmanshabbir1@gmail.com>
2023-11-14 09:48:14 +0500

    database initialization

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import os
+import sqlite3
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+import helpers
 
 
 app = Flask(__name__)
+app.config["DATABASE"] = 'db.sql'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 @app.route("/")
 def home():
