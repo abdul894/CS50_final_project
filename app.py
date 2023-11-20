036362dca99374d5695bf8b14576b1358125d4ac
abdul894 <a.rehmanshabbir1@gmail.com>
2023-11-20 15:04:09 +0500

    corrections

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 import sqlite3
 
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, g, current_app
 from flask_session import Session
-import helpers
-
 
 app = Flask(__name__)
-app.config["DATABASE"] = 'db.sql'
+app.config["DATABASE"] = 'sqlite:///fumo.db'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -22,6 +20,32 @@ def after_request(response):
     return response
 
 
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('db.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
 @app.route("/")
-def home():
+def index():
     return "Hello, World!"
+
+if __name__ == __main__:
+    app.run()
