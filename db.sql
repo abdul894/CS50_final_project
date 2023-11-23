2dea7e166754509fbb2551fdad0343ad630cbd67
abdul894 <a.rehmanshabbir1@gmail.com>
2023-11-23 22:47:42 +0500

    modified

--- db.sql
+++ db.sql
@@ -2,5 +2,6 @@ CREATE Table users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username string(255) UNIQUE NOT NULL,
     email string(255) UNIQUE NOT NULL,
-    password string(255) NOT NULL
+    password string(255) NOT NULL,
+    type string(255) NOT NULL
 )
